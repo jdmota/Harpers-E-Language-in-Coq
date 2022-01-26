7f611f61b1ecfa59255466ef738839a773f2252d
jdmota <jdmota@outlook.pt>
2022-01-26 13:25:31 +0000

    Update coq2html

--- make_website.sh
+++ make_website.sh
@@ -1,6 +1,6 @@
 cd coq
 ./compile.sh
-../coq2html-1.2/coq2html.exe *.glob *.v -base PFPL -d ../docs
+../coq2html-1.3/coq2html.exe *.glob *.v -base PFPL -d ../docs
 cd ..
 node website/generate.mjs
 node website/patch_coq_docs.mjs
